ea6f9d53aee9118711b26d6e96f8e05f9140fa2e
Rin <info@rin-m.site>
2023-09-29 12:06:14 +0900

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+// Hello World
+
+public class Hello {
+  public static void main(String[] args) {
+    String text = "Hello World";
+    System.out.println(text);
+  }
+}
---
+++ SumNumbers.java
@@ -0,0 +1,10 @@
+// Calculate the sum of two numbers.
+
+public class SumNumbers {
+  public static void main(String[] args) {
+    int a = 24;
+    int b = 16;
+    // It will return the sum of a and b.
+    System.out.println("The sum of a and b is " + Integer.sum(a, b));
+  }
+}
